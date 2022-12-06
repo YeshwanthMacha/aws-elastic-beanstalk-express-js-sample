d87fca07ffc27a7025b4203e2e3646323b81b167
Yeshwanth123 <yeshwanthmacha97@gmail.com>
2022-12-06 14:59:52 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CPSC-571-11-Cloud_COmputing_Fall-2022'));
+app.get('/', (req, res) => res.send('CPSC-571-11-Cloud_COmputing_Fall-2022_New'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
