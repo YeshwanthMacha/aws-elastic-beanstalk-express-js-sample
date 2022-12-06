c80351e2d772c5b809ed6eb9b7852bfa7822cdcc
Yeshwanth123 <yeshwanthmacha97@gmail.com>
2022-12-06 13:26:53 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('CPSC-571-11-Cloud_COmputing_Fall-2022'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
